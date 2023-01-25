55181648b2bbbd614925af5972df86308149598b
dkgo003 <smjmj1@naver.com>
2023-01-25 16:45:58 +0900

    첫 C 파일 HelloWorld

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+int main() {
+	printf("Hello, World! \n");
+	return 0;
+}
+*/
+
+
+int main() {
+	printf("  *  \n");
+	printf(" *** \n");
+	printf("***** \n");
+	return 0;
+}
